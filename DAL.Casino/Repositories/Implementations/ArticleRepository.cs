e2e9fe7031a122aea267f0701cd25ea3a673274a
Piotr Bancarzewski <piotr.bancarzewski3@gmail.com>
2018-10-03 23:09:06 +0200

    added

---
+++ DAL.Casino/Repositories/Implementations/ArticleRepository.cs
@@ -0,0 +1,15 @@
+﻿using DAL.Casino.Context;
+using DAL.Casino.Repositories.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DAL.Casino.Repositories.Implementations
+{
+    public class ArticleRepository : RepositoryBase<ArticleRepository>, IArticleRepository
+    {
+        public ArticleRepository(WebApiContext repositoryContext) : base(repositoryContext)
+        {
+        }
+    }
+}
