ec25363b5ee35767e51722e67fcc8895950f6e42
Piotr Bancarzewski <piotr.bancarzewski3@gmail.com>
2018-10-08 21:09:21 +0200

    g

--- Models.Casino/DtoModels/UserDto.cs
+++ Models.Casino/DtoModels/UserDto.cs
@@ -12,5 +12,6 @@ namespace Models.Casino.DtoModels
         public string Username { get; set; }
         public string Email { get; set; }
         public string Token { get; set; }
+        public string Password { get; set; }
     }
 }
